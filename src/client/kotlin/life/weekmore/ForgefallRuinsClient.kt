6d82a829cee6066c7e5e42c54f68ec54277bc61e
Into You <yuxiangll@foxmail.com>
2025-12-05 12:10:22 +0800

    Main Push

---
+++ src/client/kotlin/life/weekmore/ForgefallRuinsClient.kt
@@ -0,0 +1,9 @@
+package life.weekmore
+
+import net.fabricmc.api.ClientModInitializer
+
+object ForgefallRuinsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/life/weekmore/ForgefallRuinsDataGenerator.kt
@@ -0,0 +1,9 @@
+package life.weekmore
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ForgefallRuinsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
